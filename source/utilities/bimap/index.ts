638c6015854586d9f92c19d631724c60504c4fd7
Алексей Чепугов <a.chepugov@gmail.com>
2020-04-25 00:39:32 +0300

    feature(utils) bimap

---
+++ source/utilities/bimap/index.ts
@@ -0,0 +1,9 @@
+import curry from "../curry";
+import Bifunctor from "../../interfaces/Bifunctor";
+
+// bimap :: Bifunctor f => (a -> b, c -> d) -> f a c-> f b d
+export function bimap<A, B, C, D>(fnLeft: (a: A) => B, fnRight: (c: C) => D, bifunctor: Bifunctor<A, C>): Bifunctor<B, D> {
+    return bifunctor.bimap<B, D>(fnLeft, fnRight);
+}
+
+export default curry(bimap);

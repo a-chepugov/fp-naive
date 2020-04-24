418cc7cb790b781fc312b9621be54949c047e0fc
Алексей Чепугов <a.chepugov@gmail.com>
2020-04-24 22:53:36 +0300

    chore(utils) add notation to map

--- source/utilities/map/index.ts
+++ source/utilities/map/index.ts
@@ -1,6 +1,7 @@
 import curry from "../curry";
 import Functor from "../../interfaces/Functor";
 
+// map :: Functor f => (a -> b) -> f a -> f b
 export function map<A, B>(fn: (a: A) => B, functor: Functor<A>): Functor<B> {
     return functor.map<B>(fn);
 }

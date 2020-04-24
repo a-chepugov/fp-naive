4b48cedf0664c3f43b958215fbdc2241e41bde93
Алексей Чепугов <a.chepugov@gmail.com>
2020-04-24 22:55:05 +0300

    chore(utils) add notation to prop

--- source/utilities/prop/index.ts
+++ source/utilities/prop/index.ts
@@ -1,5 +1,6 @@
 import curry from "../curry";
 
+// prop :: String -> Object -> a
 export function prop<T, K extends keyof T>(target: T, key: K) {
     const {[key]: value} = target !== undefined && target !== null ? target : {};
     return value;

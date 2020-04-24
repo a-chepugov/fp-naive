f13b574127f682d7c04468873c38f9f8970c0851
Алексей Чепугов <a.chepugov@gmail.com>
2020-04-25 01:45:10 +0300

    chore(interfaces) Bifunctor

--- source/interfaces/Bifunctor.ts
+++ source/interfaces/Bifunctor.ts
@@ -1,9 +1,5 @@
-import {Functor} from './Functor';
-
-export {Functor} from './Functor';
-
-export interface Bifunctor<A, C> extends Functor<A> {
-    bimap<B, D>(fnLeft: (a: A) => B, fnRight: (c: C) => D): Bifunctor<B, D>
+export interface Bifunctor<AL, AR> {
+    bimap<BL, BR>(fnLeft: (al: AL) => BL, fnRight: (ar: AR) => BR): Bifunctor<BL, BR>
 }
 
 export default Bifunctor;

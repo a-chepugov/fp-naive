97bba4fd6713b9993e072505b18bff4a5d626e5c
Алексей Чепугов <a.chepugov@gmail.com>
2020-04-27 13:38:00 +0300

    chore(utilities) always. notation

--- source/utilities/always/index.ts
+++ source/utilities/always/index.ts
@@ -5,6 +5,7 @@
  * expect(saved()).to.equal(42);
  * expect(saved(100)).to.equal(42);
  */
+// always :: a -> b -> a
 export default function always(a: any): Function {
     return function alwaysInternal(...args: Array<any>) {
         return a;

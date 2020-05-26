42e28c3216347641ad14fbd61988af95f8aee8c5
Алексей Чепугов <a.chepugov@gmail.com>
2020-05-26 23:41:43 +0300

    feature(interfaces) Functor tests

---
+++ source/interfaces/Functor/index.test.ts
@@ -0,0 +1,32 @@
+import {expect} from "chai";
+
+import identity from "../../utilities/identity";
+import Functor from "./index";
+
+export default (Testee: any) => {
+
+    describe("Functor", () => {
+
+        it("identity", () => {
+            const value = Math.floor(Math.random() * 100);
+            const instance = new Testee(value);
+            expect(instance.map(identity).get()).to.be.equal(value);
+        });
+
+        it("composition", () => {
+            const value = Math.floor(Math.random() * 100);
+            const addition = Math.floor(Math.random() * 100);
+            const multiplier = Math.floor(Math.random() * 100);
+
+            const add = (a: number) => a + addition;
+            const mul = (a: number) => a + multiplier;
+
+            const instance = new Testee(value);
+            expect(instance.map((a: number) => mul(add(a))).get()).to.be.equal(instance.map(add).map(mul).get());
+        });
+
+    });
+
+}
+
+

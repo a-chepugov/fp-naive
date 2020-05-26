f3d2c4a83fdf47d22989b460d35ff80b540da427
Алексей Чепугов <a.chepugov@gmail.com>
2020-05-26 23:13:19 +0300

    cnore(utilities) prop

--- source/utilities/prop/index.ts
+++ source/utilities/prop/index.ts
@@ -1,5 +1,4 @@
 // prop :: String -> Object -> a
-// @ts-ignore
 export default function prop<K extends NonNullable<any>, T>(key: K) {
     return function (target: T): any {
         const {[key]: value} = target !== undefined && target !== null ? target : {};

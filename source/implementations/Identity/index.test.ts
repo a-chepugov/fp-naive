e0457fa46ccc4b6ce50a06510bf2f2e554aa4563
Алексей Чепугов <a.chepugov@gmail.com>
2020-05-28 18:59:07 +0300

    chore(implementation) Identity. add ap, chain, join

--- source/implementations/Identity/index.test.ts
+++ source/implementations/Identity/index.test.ts
@@ -6,21 +6,20 @@ import identity from "../../utilities/identity";
 
 describe("Identity", () => {
 
-    describe("Applicative", () => {
-
-        it("of returns Maybe", () => {
-            const maybe = Testee.of(5);
-            expect(maybe).to.be.instanceof(Testee);
-        });
-
-    });
-
     describe("Functor", () => {
+
+        it("map invokes on Maybe.Just", () => {
+            const instance = Testee.of(5);
+            let mapped = instance.map((value: number) => ++value);
+            expect(mapped).to.be.instanceof(Testee);
+            expect(mapped.get()).to.be.equal(6);
+        });
 
         it("identity", () => {
             const value = Math.floor(Math.random() * 100);
-            const instance = new Testee(value);
-            expect(instance.map(identity).get()).to.be.equal(value);
+            const instance = Testee.of(value);
+            const result = instance.map(identity) as Testee<number>;
+            expect(result.get()).to.be.equal(value);
         });
 
         it("composition", () => {
@@ -31,10 +30,83 @@ describe("Identity", () => {
             const add = (a: number) => a + addition;
             const mul = (a: number) => a + multiplier;
 
-            const instance = new Testee(value);
-            expect(instance.map((a: number) => mul(add(a))).get()).to.be.equal(instance.map(add).map(mul).get());
+            const instance = Testee.of(value);
+
+            const r1 = instance.map((a: number) => mul(add(a))) as Testee<number>;
+            const r2 = instance.map(add).map(mul) as Testee<number>;
+
+            expect(r1.get()).to.be.equal(r2.get());
         });
 
+    });
+
+    describe("Apply", () => {
+
+        it("ap invokes on Maybe.Just", () => {
+            const instance = Testee.of(4);
+            const add = (a: number) => a + 1;
+            const instanceAdd = Testee.of(add);
+            const resultMonad = instance.ap(instanceAdd) as Testee<number>;
+            expect(resultMonad).to.be.instanceof(Testee);
+            expect(resultMonad.get()).to.be.equal(5);
+        });
+
+        it("composition", () => {
+            const value = Math.floor(Math.random() * 100);
+            const addition = Math.floor(Math.random() * 100);
+            const multiplier = Math.floor(Math.random() * 100);
+
+            const add = (a: number) => a + addition;
+            const mul = (a: number) => a + multiplier;
+
+            const vT = Testee.of(value);
+            const aT = Testee.of(add);
+            const mT = Testee.of(mul);
+
+            const transform =
+                (f: (a: number) => number) =>
+                    (g: (a: number) => number) =>
+                        (x: number) =>
+                            f(g(x));
+
+            const r1 = vT.ap(aT.ap(mT.map(transform))) as Testee<number>;
+            const r2 = vT.ap(mT).ap(aT) as Testee<number>;
+
+            expect(r1.get()).to.be.equal(r2.get());
+        });
+
+    });
+
+    describe("Applicative", () => {
+
+        it("of returns Maybe", () => {
+            const maybe = Testee.of(5);
+            expect(maybe).to.be.instanceof(Testee);
+        });
+
+    });
+
+    describe("Chain", () => {
+
+        it("chain invokes on Maybe.Just", () => {
+            const instance = Testee.of(5);
+            let chained = instance.chain((value: number): Testee<number> => Testee.of(value + 1));
+            expect(chained).to.be.instanceof(Testee);
+            const result = chained as Testee<number>;
+            expect(result.get()).to.be.equal(6);
+        });
+
+    });
+
+    it("get", () => {
+        const instance = Testee.of(5);
+        expect(instance.get()).to.be.equal(5);
+    });
+
+    it("join", () => {
+        const instance = Testee.of(Testee.of(3));
+        expect(instance.join()).to.be.instanceof(Testee);
+        expect(instance.join().get()).to.be.equal(3);
     });
 
 });
--- source/implementations/Identity/index.ts
+++ source/implementations/Identity/index.ts
@@ -1,6 +1,6 @@
-import Functor from "../../interfaces/Functor";
+import {Apply, Chain, Monad} from "../../interfaces/Monad";
 
-export default class Identity<A> implements Functor<A> {
+export default class Identity<A> implements Monad<A> {
     private readonly value: A;
 
     constructor(value: A) {
@@ -11,9 +11,22 @@ export default class Identity<A> implements Functor<A> {
         return new Identity(fn(this.value));
     }
 
+    ap<B>(other: Apply<(value: A) => B>): Apply<B> {
+        return other.map((fn: (value: A) => B) => fn.call(this, this.value)) as Apply<B>
+    }
+
     static of<A>(value: A): Identity<A> {
         return new Identity<A>(value);
     }
+
+    chain<B>(fn: (value: A) => Chain<B>): Chain<B> {
+        return fn(this.value);
+    }
+
+    join(): A {
+        return this.value;
+    }
+
 
     get(): A {
         return this.value;

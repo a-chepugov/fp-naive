4b78a9448dd1999e58fed2b616bda8e4f522d007
Алексей Чепугов <a.chepugov@gmail.com>
2020-05-28 19:00:04 +0300

    chore(helpers) use Identity in ap & chain test

--- source/helpers/ap/index.test.ts
+++ source/helpers/ap/index.test.ts
@@ -2,14 +2,14 @@ import {expect} from "chai";
 
 import ap from "./index";
 
-import Maybe from "../../implementations/Maybe";
+import Identity from "../../implementations/Identity";
 
 describe("ap", () => {
 
     it("Maybe(inc) on Maybe(2) gives Maybe(3)", () => {
-        const maybe2 = Maybe.of(2);
-        const maybeInc = Maybe.of((a: number) => a + 1);
-        const result = ap(maybeInc, maybe2) as Maybe<number>;
+        const instance2 = Identity.of(2);
+        const instanceInc = Identity.of((a: number) => a + 1);
+        const result = ap(instanceInc, instance2) as Identity<number>;
         expect(result.get()).to.be.equal(3);
     });
 
--- source/helpers/chain/index.test.ts
+++ source/helpers/chain/index.test.ts
@@ -2,14 +2,14 @@ import {expect} from "chai";
 
 import chain from "./index";
 
-import Maybe from "../../implementations/Maybe";
+import Identity from "../../implementations/Identity";
 
 describe("chain", () => {
 
     it("chain inc on Maybe(5) gives Maybe(6)", () => {
-        const maybe = Maybe.of(5) as Maybe<number>;
-        const add2AndWrap = (value: number): Maybe<number> => Maybe.of(++value);
-        const result = chain(add2AndWrap, maybe) as Maybe<number>;
+        const instance = Identity.of(5) as Identity<number>;
+        const add2AndWrap = (value: number): Identity<number> => Identity.of(++value);
+        const result = chain(add2AndWrap, instance) as Identity<number>;
         expect(result.get()).to.be.equal(6);
     });
 

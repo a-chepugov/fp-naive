fb5604fa2061f425d77f8123f208316bf662e8c7
Алексей Чепугов <a.chepugov@gmail.com>
2020-05-31 02:02:04 +0300

    feature(utilities) liftA2

--- source/utilities/lift/index.ts
+++
@@ -1,8 +0,0 @@
-import Maybe from "../../implementations/Maybe";
-
-export default function lift<A, B>(fn: (a: A) => B): (a: A) => Maybe<B> {
-    return function (value: A): Maybe<B> {
-        const result = Maybe.of(value) as Maybe<A>;
-        return result.map(fn) as Maybe<B>;
-    }
-}

7e86f738700b8fe1035e2d4bef5a3dbd98282b1d
Алексей Чепугов <a.chepugov@gmail.com>
2020-05-30 14:20:13 +0300

    chore(helpers. concat) create test

---
+++ source/helpers/concat/index.test.ts
@@ -0,0 +1,15 @@
+import {expect} from "chai";
+
+import Testee from "./index";
+
+import List from "../../implementations/List";
+
+describe("concat", () => {
+
+    it("traverse on List([1, 2, 3]) gives 6", () => {
+        const instance1 = new List([1, 2, 3]);
+        const instance2 = new List([4, 5, 6]);
+        expect((Testee(instance1, instance2) as List<number>).get()).to.be.deep.equal([1, 2, 3, 4, 5, 6]);
+    });
+
+});

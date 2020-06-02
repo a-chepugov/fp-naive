4604fc9e4bcee2e632511320cfc7cf73711de619
Алексей Чепугов <a.chepugov@gmail.com>
2020-06-02 11:06:49 +0300

    chore (utilities. prop) fix signature

--- source/utilities/prop/index.ts
+++ source/utilities/prop/index.ts
@@ -1,5 +1,5 @@
-// prop :: String -> Object -> a
-export default function prop<K extends NonNullable<any>, T>(key: K) {
+// prop :: String | Number | Symbol -> Object -> a
+export default function prop<K extends NonNullable<string | number | symbol>, T>(key: K) {
     return function (target: T): any {
         const {[key]: value} = target !== undefined && target !== null ? target : {};
         return value;

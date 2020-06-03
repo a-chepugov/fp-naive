fc3f95b9a9c2ceb424ed4130ec79b4bc7dd57cc3
Алексей Чепугов <a.chepugov@gmail.com>
2020-06-03 21:30:21 +0300

    chore(interfaces. Apply) remove unnecessary tests

--- source/interfaces/Apply/index.test.ts
+++ source/interfaces/Apply/index.test.ts
@@ -1,35 +1,8 @@
-import identity from "../../utilities/identity";
-
 const compose = (f: any) => (g: any) => (x: any) => f(g(x));
 
 export default (M: any, {x, f, g}: {x: any, f: any, g: any}, assert: { equal: any }) => {
 
     describe("Apply", () => {
-
-        it("identity", () => {
-            const v = M.of(x);
-
-            const r1 = v;
-            const r2 = M.of(identity).ap(v);
-
-            return assert.equal(r1, r2);
-        });
-
-        it("homomorphism", () => {
-            const r1 = M.of(f).ap(M.of(x));
-            const r2 = M.of(f(x));
-
-            return assert.equal(r1, r2);
-        });
-
-        it("interchange", () => {
-            const v = M.of(f);
-
-            const r1 = v.ap(M.of(x));
-            const r2 = M.of((f: (a: any) => any) => f(x)).ap(v);
-
-            return assert.equal(r1, r2);
-        });
 
         it("composition", () => {
             const u = M.of(f);

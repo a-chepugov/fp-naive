b6a8bf4d27b1274ea1b82ee12ce8845789ce1210
Алексей Чепугов <a.chepugov@gmail.com>
2020-06-03 11:12:25 +0300

    chore(helpers) unnecessary generic types removed

--- source/helpers/chain/index.ts
+++ source/helpers/chain/index.ts
@@ -1,5 +1,5 @@
 import Chain from "../../interfaces/Chain";
 
 export default function chain<A, B>(fn: (a: A) => Chain<B>, chain: Chain<A>): Chain<B> {
-    return chain.chain<B>(fn);
+    return chain.chain(fn);
 }
--- source/helpers/map/index.ts
+++ source/helpers/map/index.ts
@@ -1,5 +1,5 @@
 import Functor from "../../interfaces/Functor";
 
 export default function map<A, B>(fn: (a: A) => B, functor: Functor<A>): Functor<B> {
-    return functor.map<B>(fn);
+    return functor.map(fn);
 }

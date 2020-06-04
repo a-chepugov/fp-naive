61f3c479aabcc7621c8924a16c109205f7e95f65
Алексей Чепугов <a.chepugov@gmail.com>
2020-06-04 23:41:02 +0300

    feature(implementations) Compose primitive test

---
+++ source/implementations/Compose/index.test.ts
@@ -0,0 +1,19 @@
+import {expect} from "chai";
+
+import Testee from "./index";
+import Identity from "../Identity";
+import Maybe from "../Maybe";
+
+describe("Compose", () => {
+
+    it("Build", () => {
+        const Factory = Testee(Identity, Maybe);
+        const compose = Factory.of(5);
+        expect(compose.get()).to.be.instanceof(Identity);
+        // @ts-ignore
+        expect(compose.get().get()).to.be.instanceof(Maybe);
+        // @ts-ignore
+        expect(compose.get().get().get()).to.be.deep.equal(5);
+    });
+
+});

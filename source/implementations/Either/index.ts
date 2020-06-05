7e288a675cbbc56e74978237bda269336d9c4f5d
Алексей Чепугов <a.chepugov@gmail.com>
2020-06-05 12:29:15 +0300

    chore(implementations. Either) delete redundant undefined from constructors signatures

--- source/implementations/Either/index.ts
+++ source/implementations/Either/index.ts
@@ -22,7 +22,7 @@ export default abstract class Either<L, R> implements Monad<R>, Bifunctor<L, R>,
     abstract chain<R2>(fn: (right: R) => Either<L, R2>): Either<L, R2>;
 
     static of<L, R>(right: R): Either<L, R> {
-        return new Right<L, R>(undefined, right);
+        return new Right<L, R>(right);
     }
 
     abstract bimap<L2, R2>(fnLeft: (left: L) => L2, fnRight: (right: R) => R2): Either<L2, R2>;
@@ -38,11 +38,11 @@ export default abstract class Either<L, R> implements Monad<R>, Bifunctor<L, R>,
     abstract getOrElse(other: R): R;
 
     static right<L, R>(right: R): Right<L, R> {
-        return new Right<L, R>(undefined, right);
+        return new Right<L, R>(right);
     }
 
     static left<L, R>(left: L): Left<L, R> {
-        return new Left<L, R>(left, undefined);
+        return new Left<L, R>(left);
     }
 
     abstract swap(): Either<R, L>;
@@ -66,25 +66,25 @@ export default abstract class Either<L, R> implements Monad<R>, Bifunctor<L, R>,
 class Left<L, R> extends Either<L, R> {
     private readonly value: L;
 
-    constructor(left: L, _right: R) {
+    constructor(value: L) {
         super();
-        this.value = left;
+        this.value = value;
     }
 
     map<R2>(fn: (a: R) => R2): Left<L, R2> {
-        return new Left<L, R2>(this.value, undefined);
+        return new Left<L, R2>(this.value);
     }
 
     ap<R2>(other: Either<any, R2>): R extends FNA1<R2, infer R3> ? Either<L, R3> : Either<any, any> {
-        return new Left(this.value, undefined) as (R extends FNA1<R2, infer R3> ? Left<L, R3> : Left<any, any>);
+        return new Left(this.value) as (R extends FNA1<R2, infer R3> ? Left<L, R3> : Left<any, any>);
     }
 
     chain<R2>(fn: (right: R) => Either<L, R2>): Either<L, R2> {
-        return new Left<L, R2>(this.value, undefined);
+        return new Left<L, R2>(this.value);
     }
 
     bimap<L2, R2>(fnLeft: (left: L) => L2, fnRight: (right: R) => R2): Left<L2, R2> {
-        return new Left<L2, R2>(fnLeft(this.value), undefined);
+        return new Left<L2, R2>(fnLeft(this.value));
     }
 
     reduce<R2>(reducer: (accumulator: R2, value: R) => R2, initial: R2): R2 {
@@ -92,11 +92,11 @@ class Left<L, R> extends Either<L, R> {
     }
 
     traverse<R2>(TypeRep: ApplicativeTypeRep<Either<L, R2>>, fn: (a: R) => Applicative<R2>): Applicative<Either<L, R2>> {
-        return TypeRep.of(new Left<L, R2>(undefined, undefined));
+        return TypeRep.of(new Left<L, R2>(undefined));
     }
 
     mapLeft<L2>(fn: (left: L) => L2): Left<L2, R> {
-        return new Left<L2, R>(fn(this.value), undefined);
+        return new Left<L2, R>(fn(this.value));
     }
 
     join(): Either<L, R> {
@@ -112,7 +112,7 @@ class Left<L, R> extends Either<L, R> {
     }
 
     swap(): Either<R, L> {
-        return new Right<R, L>(undefined, this.value);
+        return new Right<R, L>(this.value);
     }
 
     get isLeft(): Boolean {
@@ -134,13 +134,13 @@ class Left<L, R> extends Either<L, R> {
 class Right<L, R> extends Either<L, R> {
     private readonly value: R;
 
-    constructor(_left: L, right: R) {
+    constructor(value: R) {
         super();
-        this.value = right;
+        this.value = value;
     }
 
     map<R2>(fn: (a: R) => R2): Either<L, R2> {
-        return new Right<L, R2>(undefined, fn(this.value));
+        return new Right<L, R2>(fn(this.value));
     }
 
     ap<R2>(other: Either<any, R2>): R extends FNA1<R2, infer R3> ? Either<L, R3> : Either<any, any> {
@@ -156,11 +156,11 @@ class Right<L, R> extends Either<L, R> {
     }
 
     bimap<L2, R2>(fnLeft: (left: L) => L2, fnRight: (right: R) => R2): Right<L2, R2> {
-        return new Right<L2, R2>(undefined, fnRight(this.value));
+        return new Right<L2, R2>(fnRight(this.value));
     }
 
     mapLeft<L2>(fn: (left: L) => L2): Either<L2, R> {
-        return new Right<L2, R>(undefined, this.value);
+        return new Right<L2, R>(this.value);
     }
 
     reduce<R2>(reducer: (accumulator: R2, value: R) => R2, initial: R2): R2 {
@@ -168,7 +168,7 @@ class Right<L, R> extends Either<L, R> {
     }
 
     traverse<R2>(TypeRep: ApplicativeTypeRep<Either<L, R2>>, fn: (a: R) => Applicative<R2>): Applicative<Either<L, R2>> {
-        return fn(this.value).map((a: R2) => new Right<L, R2>(undefined, a));
+        return fn(this.value).map((a: R2) => new Right<L, R2>(a));
     }
 
     join(): Either<L, R | any> {
@@ -186,7 +186,7 @@ class Right<L, R> extends Either<L, R> {
     }
 
     swap(): Either<R, L> {
-        return new Left<R, L>(this.value, undefined);
+        return new Left<R, L>(this.value);
     }
 
     get isRight(): Boolean {

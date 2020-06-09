8f7658bc920ee99f40231661778a7ec0a141cd1d
Алексей Чепугов <a.chepugov@gmail.com>
2020-06-09 14:18:11 +0300

    feature. Natural Transformation specifications tests

---
+++ source/specifications/nt/index.test.ts
@@ -0,0 +1,15 @@
+export default (nt: any, x: any, fn: any, assert: { equal: any }) => {
+
+    describe("Natural transformation", () => {
+
+        it('morphism', () => {
+
+            const r1 = nt(x).map(fn);
+            const r2 = nt(x.map(fn));
+
+            return assert.equal(r1, r2);
+        });
+
+    });
+
+}

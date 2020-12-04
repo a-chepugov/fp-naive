debce4d62d0e612754b13c1412633dd0e2943d78
Алексей Чепугов <a.chepugov@gmail.com>
2020-12-04 13:02:37 +0200

    chore (tap) test minor corrections

--- source/helpers/tap/index.test.ts
+++ source/helpers/tap/index.test.ts
@@ -8,16 +8,15 @@ describe("tap", () => {
 	it("interceptor result is omitted", () => {
 		const interceptor = (a: number) => a + 1;
 
-		const tap1 = tap(interceptor);
-		expect(tap1(1)).to.be.equal(1);
+		const tapped = tap(interceptor);
+		expect(tapped(1)).to.be.equal(1);
 	});
 
 	it("can be used in chain", () => {
 		let accumulator = 0;
-		const tapFn = tap((a: number) => accumulator += a);
 
 		Maybe.of(5)
-			.map(tapFn);
+			.map(tap((a: number) => accumulator += a));
 
 		expect(accumulator).to.be.equal(5);
 	});
